a012e96fb730dece7b15d6d57bfbd715aac10f81
ryabchikk <mixail-12-r@mail.ru>
2022-12-23 15:09:26 +0300

    create project

---
+++ SimpleProject/SimpleProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SimpleProject
+{
+    class Program
+    {
+        static public int MultiplyTwoNumbers(int a, int b)
+        {
+            int res = a * b;
+            return res;    
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello EPTA!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(MultiplyTwoNumbers(a, b));
+        }
+    }
+}
